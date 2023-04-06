e83bea8b152b8d151aef52ba6527b4aea645b5d5
BlackyDrum <ganiaytan1010@web.de>
2023-04-07 00:30:13 +0200

    added total price to cart

--- public/js/articles.js
+++ public/js/articles.js
@@ -30,6 +30,7 @@ for(let i = 0; i < document.getElementsByClassName('addButtons').length; i++)
         articlePrice.innerHTML = arr[2] + "€";
         row.appendChild(articlePrice);
 
+        let removeButton = document.createElement('td');
         let remove = document.createElement('button');
         remove.innerHTML = "<i class=\"fa-solid fa-xmark\"></i>";
         remove.value = arr[0]; // ID vom Artikel
@@ -37,17 +38,41 @@ for(let i = 0; i < document.getElementsByClassName('addButtons').length; i++)
         remove.addEventListener('click', function () {
             document.getElementById(arr[0]).style.opacity = "1";
             buttons[i].disabled = false;
-            remove.parentElement.remove();
+            remove.parentElement.parentElement.remove();
             if (document.getElementById('cart-table').childElementCount === 1) {
                 document.getElementById('cart-info').style.display = "initial";
                 document.getElementById('cart-cond').value = "empty";
                 document.getElementById('cart-table').style.display = "none";
+                document.getElementById('sum').remove();
             }
+            setSum();
         });
-        row.appendChild(remove);
 
+        removeButton.appendChild(remove);
+        row.appendChild(removeButton);
+
+        if (!document.getElementById('sum'))
+        {
+            let sum = document.createElement('p');
+            sum.innerHTML = "Gesamtpreis: ";
+            sum.className = "font-bold text-2xl m-3";
+            sum.id = "sum";
+            document.getElementById('cart').appendChild(sum);
+            console.log(sum);
+        }
         document.getElementById('cart-table').appendChild(row);
+
+        setSum();
     });
+}
+
+function setSum() {
+    let sum = 0;
+    for (let i = 1; i < document.getElementById('cart-table').childElementCount; i++)
+    {
+        sum += parseInt(document.getElementById('cart-table').children[i].children[1].innerHTML.replace('€',''));
+    }
+    document.getElementById('sum').innerHTML = "Gesamtpreis: " +  sum + "€";
 }
 
 
