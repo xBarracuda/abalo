26e70892312e118a5a340b44d5c6d029d1041d61
BlackyDrum <ganiaytan1010@web.de>
2023-04-07 13:36:49 +0200

    fixed mobile viewport bug

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -12,29 +12,26 @@
 @section('main')
     <div class="text-center my-5">
         @if($articles && count($articles) != 0)
-            @if(count($articles) <= 3)
-                <div class="h-96 mb-40">
-                    @endif
-                    <div class="flex justify-evenly flex-wrap">
-                        @foreach($articles as $a)
-                            <div class="card m-10 w-48" id="{{$a->id}}">
-                                <img
-                                    src="@if(file_exists('img/'.$a->id.'.png')) {{asset('img/'.$a->id.'.png')}} @elseif(file_exists(('img/'.$a->id.'.jpg'))) {{asset('img/'.$a->id.'.jpg')}} @endif"
-                                    class="" alt="Avatar">
-                                <div class="container">
-                                    <h4><b>{{$a->ab_name}}</b></h4>
-                                    <p>{{number_format($a->ab_price,2)}}€</p>
-                                    <p class="py-5 h-40">{{$a->ab_description}}</p>
-                                </div>
-                                <p class="bg-gray-600/50">
-                                    <button class="addButtons" value="{{$a->id}}/??/{{$a->ab_name}}/??/{{$a->ab_price}}/??/{{$a->ab_description}}">Zum Warenkorb hinzufügen</button>
-                                </p>
-                            </div>
-                        @endforeach
+            <div class="flex justify-evenly flex-wrap">
+                @foreach($articles as $a)
+                    <div class="card m-10 w-48" id="{{$a->id}}">
+                        <img
+                            src="@if(file_exists('img/'.$a->id.'.png')) {{asset('img/'.$a->id.'.png')}} @elseif(file_exists(('img/'.$a->id.'.jpg'))) {{asset('img/'.$a->id.'.jpg')}} @endif"
+                            class="" alt="Avatar">
+                        <div class="container">
+                            <h4><b>{{$a->ab_name}}</b></h4>
+                            <p>{{number_format($a->ab_price,2)}}€</p>
+                            <p class="py-5 h-40">{{$a->ab_description}}</p>
+                        </div>
+                        <p class="bg-gray-600/50">
+                            <button class="addButtons"
+                                    value="{{$a->id}}/??/{{$a->ab_name}}/??/{{$a->ab_price}}/??/{{$a->ab_description}}">
+                                Zum Warenkorb hinzufügen
+                            </button>
+                        </p>
                     </div>
-                    @if(count($articles) <= 3)
-                </div>
-            @endif
+                @endforeach
+            </div>
         @elseif($articles && count($articles) == 0)
             <div class="h-80 flex">
                 <h1 class="my-auto mx-auto font-bold text-3xl">Es tut uns leid, Ihre Anfrage hat keinen Treffer ergeben
@@ -53,7 +50,10 @@
                             <p class="py-5 h-40">{{$a->ab_description}}</p>
                         </div>
                         <p class="bg-gray-600/50">
-                            <button class="addButtons" value="{{$a->id}}/??/{{$a->ab_name}}/??/{{$a->ab_price}}/??/{{$a->ab_description}}">Zum Warenkorb hinzufügen</button>
+                            <button class="addButtons"
+                                    value="{{$a->id}}/??/{{$a->ab_name}}/??/{{$a->ab_price}}/??/{{$a->ab_description}}">
+                                Zum Warenkorb hinzufügen
+                            </button>
                         </p>
                     </div>
                 @endforeach
