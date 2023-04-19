8cd44ac0a279615e14cfe189567d1ea33aeb7371
BlackyDrum <ganiaytan1010@web.de>
2023-04-19 15:09:49 +0200

    added array-object structure for navigation menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,191 +13,104 @@
 <body class="font-mono" onload="init()">
 @section('header')
     <header id="header">
-        <script>
-            let OuterContainer = document.createElement('div');
-            OuterContainer.className = "w-full mb-5 h-34 bg-gray-300/50 grid grid-cols-2 grid-cols-[20%,80%]";
-            document.getElementById('header').appendChild(OuterContainer);
+        <div class="w-full mb-5 h-34 bg-gray-300/50 grid grid-cols-2 grid-cols-[20%,80%]">
+            <div>
+                <a href="/"><img src="{{asset('img/abalo-logos.png')}}" width="80" class=" mx-auto"></a>
+            </div>
+            <div class="mr-16 max-xl:hidden" id="navigationItems">
+                <div class="flex justify-evenly align-items-center" id="innerNav">
+                    <script>
+                        const menu = [
+                            { name : "Home", url : "/", sub : [], routeName : "home" },
+                            { name : "Kategorien", url : "/category", sub : [], routeName : "category" },
+                            { name : "Artikel", url : "/articles", sub : [], routeName : "articles" },
+                            { name : "Verkaufen", url : "/newarticle", sub : [], routeName : "sell" },
+                            { name : "Unternehmen", url : "/about", sub : [ { name : "Philosophie", url : "/about#philosophy"}, { name : "Karriere", url : "/about#career"}], routeName : "about" },
+                            { name : "Kontakt", url : "/contact", sub : [], routeName : "contact" },
+                        ];
 
-            let imageContainer = document.createElement('div');
-            let logo = document.createElement('a');
-            logo.href = "/";
-            let logoImg = document.createElement('img');
-            logoImg.src = "{{asset('img/abalo-logos.png')}}";
-            logoImg.className = "mx-auto";
-            logoImg.width = 80;
-            logo.appendChild(logoImg);
-            imageContainer.appendChild(logo);
-            OuterContainer.appendChild(imageContainer);
+                        let currentLocation = "{{\Illuminate\Support\Facades\Route::getCurrentRoute()->getName()}}";
 
-            let navContainer = document.createElement('div');
-            navContainer.className = "mr-16 max-xl:hidden";
-            navContainer.id = "navigationItems";
-            OuterContainer.appendChild(navContainer);
+                        for (const obj of menu)
+                        {
+                            var containerNav = document.createElement('div');
+                            containerNav.className = "mt-8";
+                            document.getElementById('innerNav').append(containerNav);
 
-            let navInnerContainer = document.createElement('div');
-            navInnerContainer.className = "flex justify-evenly align-items-center";
-            navContainer.appendChild(navInnerContainer);
+                            var url = document.createElement('a');
+                            url.innerText = obj.name;
+                            url.href = obj.url;
 
-            let navItemContainer0 = document.createElement('div');
-            navItemContainer0.className = "mt-8";
-            navInnerContainer.appendChild(navItemContainer0);
+                            url.className = (currentLocation === obj.routeName) ? "hover:underline underline decoration-1 font-bold hover:font-bold" : "hover:underline hover:font-bold";
 
-            let navItem0 = document.createElement('a');
-            navItem0.href = "/";
-            navItem0.className = "hover:underline @if(\Illuminate\Support\Facades\Route::getCurrentRoute()->getName() == 'home') underline decoration-1 font-bold @endif hover:font-bold";
-            navItem0.innerText = "Home";
-            navItemContainer0.appendChild(navItem0);
+                            containerNav.append(url);
 
-            let navItemContainer1 = document.createElement('div');
-            navItemContainer1.className = "mt-8";
-            navInnerContainer.appendChild(navItemContainer1);
+                            if (obj.sub.length > 0)
+                            {
+                                containerNav.id = obj.url.replace('/','');
+                                containerNav.className = "mt-8 relative";
 
-            let navItem1 = document.createElement('a');
-            navItem1.href = "/category";
-            navItem1.className = "hover:underline @if(\Illuminate\Support\Facades\Route::getCurrentRoute()->getName() == 'category') underline decoration-1 font-bold @endif hover:font-bold";
-            navItem1.innerText = "Kategorien";
-            navItemContainer1.appendChild(navItem1);
+                                let subContainer = document.createElement('div');
+                                subContainer.id = obj.url.replace('/','') + "Bar";
+                                subContainer.className = "w-full bg-gray-300/50 absolute";
+                                containerNav.append(subContainer);
 
-            let navItemContainer2 = document.createElement('div');
-            navItemContainer2.className = "mt-8";
-            navInnerContainer.appendChild(navItemContainer2);
+                                let list = document.createElement('ul');
+                                list.className = "list-disc";
+                                subContainer.append(list);
 
-            let navItem2 = document.createElement('a');
-            navItem2.href = "/articles";
-            navItem2.className = "hover:underline @if(\Illuminate\Support\Facades\Route::getCurrentRoute()->getName() == 'articles') underline decoration-1 font-bold @endif hover:font-bold";
-            navItem2.innerText = "Artikel";
-            navItemContainer2.appendChild(navItem2);
+                                for (const element of obj.sub)
+                                {
+                                    let url = document.createElement('a');
+                                    url.href = element.url;
+                                    url.className = "hover:underline";
+                                    list.append(url);
 
-            let navItemContainer3 = document.createElement('div');
-            navItemContainer3.className = "mt-8";
-            navInnerContainer.appendChild(navItemContainer3);
+                                    let item = document.createElement('li');
+                                    item.innerText = element.name;
+                                    url.append(item);
+                                }
+                            }
+                        }
+                    </script>
 
-            let navItem3 = document.createElement('a');
-            navItem3.href = "/newarticle";
-            navItem3.className = "hover:underline @if(\Illuminate\Support\Facades\Route::getCurrentRoute()->getName() == 'sell') underline decoration-1 font-bold @endif hover:font-bold";
-            navItem3.innerText = "Verkaufen";
-            navItemContainer3.appendChild(navItem3);
+                    <div class="mt-8">
+                        <button onclick="searchbar()"><i class="fa-solid fa-magnifying-glass"></i></button>
+                    </div>
+                    <div class="mt-8">
+                        <button id="cart-icon" class=""><i class="fa-solid fa-cart-shopping"></i> <span
+                                id="cart-counter" class="border border-black rounded-full p-0.5">0</span></button>
+                    </div>
+                    <div class="mt-8">
+                        @if(!session()->has('abalo_user'))
+                            <a href="/login" class="hover:underline hover:font-bold"><i
+                                    class="fa-solid fa-user"></i></a>
+                        @else
+                            <a href="/profile"
+                               class="font-bold hover:underline hover:font-bold">Willkommen {{session()->get('abalo_user')}}</a>
+                        @endif
+                    </div>
+                    @if(session()->has('abalo_user'))
+                        <div class="mt-8">
+                            <a href="/logout" class="hover:underline hover:font-bold">Ausloggen</a>
+                        </div>
+                    @endif
+                </div>
+            </div>
+            <div class="absolute w-1/2 left-1/2 my-6 max-xl:hidden grid grid-cols-2" id="searchbar">
+                <form method="get" action="/articles">
+                    <input type="text" name="search" class="rounded-xl w-full" placeholder="Ihr Suchbegriff"
+                           autocomplete="off">
+                </form>
+                <div class="m-auto">
+                    <button id="searchbarClose" onclick="searchbar()"></button>
+                </div>
+            </div>
+            <div class="mr-16 xl:hidden flex">
+                <button id="nav" class="ml-auto" onclick="navbar()"><i class="fa-solid fa-bars scale-125"></i></button>
+            </div>
 
-            let navItemContainer4 = document.createElement('div');
-            navItemContainer4.className = "mt-8 relative";
-            navItemContainer4.id = "about";
-            navInnerContainer.appendChild(navItemContainer4);
-
-            let navItem4 = document.createElement('a');
-            navItem4.href = "/about";
-            navItem4.className = "hover:underline @if(\Illuminate\Support\Facades\Route::getCurrentRoute()->getName() == 'about') underline decoration-1 font-bold @endif hover:font-bold";
-            navItem4.innerText = "Unternehmen";
-            navItemContainer4.appendChild(navItem4);
-            let aboutBar = document.createElement('div');
-            aboutBar.id = "aboutBar";
-            aboutBar.className = "w-full bg-gray-300/50 absolute";
-            navItemContainer4.appendChild(aboutBar);
-            let aboutList = document.createElement('ul');
-            aboutList.className = "list-disc";
-            aboutBar.appendChild(aboutList);
-            let aboutHref0 = document.createElement('a');
-            aboutHref0.href = "/about#philosophy";
-            aboutHref0.className = "hover:underline";
-            aboutList.appendChild(aboutHref0);
-            let aboutHref1 = document.createElement('a');
-            aboutHref1.href = "/about#career";
-            aboutHref1.className = "hover:underline";
-            aboutList.appendChild(aboutHref1);
-            let aboutItem0 = document.createElement('li');
-            aboutItem0.innerText = "Philosophie";
-            aboutHref0.appendChild(aboutItem0);
-            let aboutItem1 = document.createElement('li');
-            aboutItem1.innerText = "Karriere";
-            aboutHref1.appendChild(aboutItem1);
-
-            let navItemContainer5 = document.createElement('div');
-            navItemContainer5.className = "mt-8";
-            navInnerContainer.appendChild(navItemContainer5);
-
-            let navItem5 = document.createElement('a');
-            navItem5.href = "/contact";
-            navItem5.className = "hover:underline @if(\Illuminate\Support\Facades\Route::getCurrentRoute()->getName() == 'contact') underline decoration-1 font-bold @endif hover:font-bold";
-            navItem5.innerText = "Kontakt";
-            navItemContainer5.appendChild(navItem5);
-
-            let navItemContainer6 = document.createElement('div');
-            navItemContainer6.className = "mt-8";
-            navInnerContainer.appendChild(navItemContainer6);
-
-            let navItem6 = document.createElement('button');
-            navItem6.onclick = function () {
-                if (document.getElementById('navigationItems').hidden === true) {
-                    document.getElementById('navigationItems').hidden = false;
-                    document.getElementById('searchbar').style.visibility = "hidden";
-                    document.getElementById('searchbarClose').innerHTML = "";
-                    return;
-                }
-
-                document.getElementById('searchbarClose').innerHTML = "<i class=\"fa-solid fa-xmark scale-150\"></i>";
-                document.getElementById('navigationItems').hidden = true;
-                document.getElementById('searchbar').style.visibility = "visible";
-            }
-            navItemContainer6.appendChild(navItem6);
-            let searchIcon = document.createElement('i');
-            searchIcon.className = "fa-solid fa-magnifying-glass";
-            navItem6.appendChild(searchIcon);
-
-
-            let navItemContainer7 = document.createElement('div');
-            navItemContainer7.className = "mt-8";
-            navInnerContainer.appendChild(navItemContainer7);
-
-            let navItem7 = document.createElement('button');
-            navItem7.id = "cart-icon";
-            navItemContainer7.appendChild(navItem7);
-            let cartIcon = document.createElement('i');
-            cartIcon.className = "fa-solid fa-cart-shopping";
-            navItem7.appendChild(cartIcon);
-            let counter = document.createElement('span');
-            counter.id = "cart-counter";
-            counter.className = "border border-black rounded-full mx-1 p-0.5";
-            counter.innerText = "0";
-            navItem7.appendChild(counter);
-
-            let navItemContainer8 = document.createElement('div');
-            navItemContainer8.className = "mt-8";
-            navItemContainer8.innerHTML = '@if(!session()->has('abalo_user')) <a href="/login" class="hover:underline hover:font-bold"><i class="fa-solid fa-user"></i></a> @else <a href="/profile"  class="font-bold hover:underline hover:font-bold">Willkommen {{session()->get('abalo_user')}}</a> @endif';
-            navInnerContainer.appendChild(navItemContainer8);
-
-            let navItemContainer9 = document.createElement('div');
-            navItemContainer9.className = "mt-8";
-            navItemContainer9.innerHTML = '@if(session()->has('abalo_user')) <div> <a href="/logout" class="hover:underline hover:font-bold">Ausloggen</a> </div> @endif';
-            navInnerContainer.appendChild(navItemContainer9);
-
-            let searchbarContainer = document.createElement('div');
-            searchbarContainer.className = "absolute w-1/2 left-1/2 my-6 max-xl:hidden grid grid-cols-2";
-            searchbarContainer.id = "searchbar";
-            OuterContainer.appendChild(searchbarContainer);
-
-            let searchForm = document.createElement('form');
-            searchForm.method = "get";
-            searchForm.action = "/articles";
-            let searchInput = document.createElement('input');
-            searchInput.type = "text";
-            searchInput.name = "search";
-            searchInput.className = "rounded-xl w-full";
-            searchInput.placeholder = "Ihr Suchbegriff";
-            searchInput.autocomplete = "off";
-            searchForm.appendChild(searchInput);
-            searchbarContainer.appendChild(searchForm);
-
-            let closeSearch = document.createElement('div');
-            closeSearch.className = "m-auto";
-            closeSearch.innerHTML = '<button id="searchbarClose" onclick="searchbar()"></button>';
-            searchbarContainer.appendChild(closeSearch);
-
-            let navbar0 = document.createElement('div');
-            navbar0.className = "mr-16 xl:hidden flex";
-            navbar0.innerHTML = '<button id="nav" class="ml-auto" onclick="navbar()"><i class="fa-solid fa-bars scale-125"></i></button>';
-            OuterContainer.appendChild(navbar0);
-
-        </script>
-
+        </div>
     </header>
     <div id="navbar" class="w-full bg-gray-400 transition-all xl:hidden">
         <div id="navitems"
